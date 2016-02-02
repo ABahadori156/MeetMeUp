d8da89aea9e6df34fa62a27c48e919d78da67d83
Aaron Bikis <aaron.bikis@gmail.com>
2016-02-01 16:48:21 -0800

    Aaron's Revision

---
+++ MeetMeUp/webVC.swift
@@ -0,0 +1,35 @@
+//
+//  webVC.swift
+//  MeetMeUp
+//
+//  Created by Aaron B on 2/1/16.
+//  Copyright © 2016 Rafael Auriemo. All rights reserved.
+//
+
+import UIKit
+
+class webVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
